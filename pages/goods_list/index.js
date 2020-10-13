91ea47ca8204dd599de54637a2c3a36fcbd180bc
wxd <1360273288@qq.com>
2020-10-13 17:13:36 +0800

    商品列表

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,66 +1,75 @@
-// pages/goods_list/index.js
+import { request } from "../../request/index.js";
+import regeneratorRuntime from '../../lib/runtime/runtime';
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    tabs:[
+      {
+        id: 0,
+        value: "综合",
+        isActive: true
+      },
+      {
+        id: 0,
+        value: "销量",
+        isActive: false
+      },
+      {
+        id: 0,
+        value: "价格",
+        isActive: false
+      }
+    ],
+    goodsList: []
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  QueryParams: {
+    query: "",
+    cid: "",
+    pagenum: 1,
+    pagesize: 10
+  },
+
+  totalPages: 1,
+
   onLoad: function (options) {
-
+    this.QueryParams.cid = options.cid
+    this.getGoodsList()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleTabsItemChange(e) {
+    const { index } = e.detail
+    const { tabs } = this.data
+    tabs.forEach((x,i) => i === index ? x.isActive=true : x.isActive=false)
+    this.setData({
+      tabs
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  async getGoodsList() {
+    const res = await request({url: "/goods/search", data: this.QueryParams})
+    const total = res.total
+    this.totalPages = Math.ceil(total/this.QueryParams.pagesize)
+    this.setData({
+      goodsList: [...this.data.goodsList,...res.goods]
+    })
+    wx.stopPullDownRefresh();
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  onReachBottom() {
+    if(this.QueryParams.pagenum>=this.totalPages) {
+      wx.showToast({ 
+        title: '没有更多数据了',
+        icon: "none"
+     });
+    } else{
+      this.QueryParams.pagenum++;
+      this.getGoodsList();
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onPullDownRefresh() {
+    this.setData({
+      goodsList: []
+    })
+    this.QueryParams.pagenum = 1
+    this.getGoodsList()
   }
 })
--- request/index.js
+++ request/index.js
@@ -1,16 +1,27 @@
+let ajaxTimes = 0;
 export const request = (params) => {
-    const baseUrl = 'https://api-hmugo-web.itheima.net/api/public/v1'
-    return new Promise((resolve,reject) => {
-        wx.request({
-            ...params,
-            url: baseUrl + params.url,
-            success: (result) => {
-                resolve(result.data.message);
-            },
-            fail: (err) => {
-                reject(err);
-            }
-        })
-
-    })
-}
+  ajaxTimes++
+  wx.showLoading({
+    title: "加载中",
+    mask: true,
+  });
+  const baseUrl = "https://api-hmugo-web.itheima.net/api/public/v1";
+  return new Promise((resolve, reject) => {
+    wx.request({
+      ...params,
+      url: baseUrl + params.url,
+      success: (result) => {
+        resolve(result.data.message);
+      },
+      fail: (err) => {
+        reject(err);
+      },
+      complete: () => {
+        ajaxTimes--;
+        if (ajaxTimes === 0) {
+          wx.hideLoading();
+        }
+      },
+    });
+  });
+};
