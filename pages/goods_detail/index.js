04ab8ae969ee15b9991b38ac22b47b63476ef2dd
wxd <1360273288@qq.com>
2020-10-13 18:24:31 +0800

    商品详情

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,55 @@
-// pages/goods_detail/index.js
+import { request } from "../../request/index.js";
+import regeneratorRuntime from '../../lib/runtime/runtime';
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    goodsObj: {}
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  GoodsInfo: {},
+
   onLoad: function (options) {
-
+    const { goods_id } = options
+    this.getGoodsDetail(goods_id)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  async getGoodsDetail(goods_id) {
+    const goodsObj = await request({url:'/goods/detail', data: {goods_id}})
+    this.GoodsInfo = goodsObj
+    this.setData({
+      goodsObj: {
+        goods_name: goodsObj.goods_name,
+        goods_price: goodsObj.goods_price,
+        goods_introduce: goodsObj.goods_introduce.replace(/\.webp/g, '.jpg'),
+        pics: goodsObj.pics
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handlePrevewImage(e) {
+    const urls = this.GoodsInfo.pics.map(x => x.pics_mid)
+    const current = e.currentTarget.dataset.url
+    wx.previewImage({
+      urls,
+      current
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+  handleCartAdd() {
+    let cart = wx.getStorageSync('cart')||[]
+    let index = cart.findIndex(x => x.goods_id === this.GoodsInfo.goods_id)
+    if(index === -1) {
+      this.GoodsInfo.num = 1
+      cart.push(this.GoodsInfo)
+    } else {
+      cart[index].num++
+    }
+    wx.setStorageSync('cart', cart)
 
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    wx.showToast({
+      title: '加入购物车成功',
+      icon: 'success',
+      mask: true
+    });
   }
 })
