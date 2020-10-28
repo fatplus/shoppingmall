01618bd1ac252724d5ec3eb1f2222c3f4afbff23
wxd <1360273288@qq.com>
2020-10-28 18:34:55 +0800

    搜索功能

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -13,9 +13,9 @@ Page({
     const { code } = await login();
     const loginParams={ encryptedData, rawData, iv, signature ,code};
     //  3 发送请求 获取用户的token
-    const {token}=await request({url:"/users/wxlogin",data:loginParams,method:"post"});
+    // const {token}=await request({url:"/users/wxlogin",data:loginParams,method:"post"});
     // 4 把token存入缓存中 同时跳转回上一个页面
-    // const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjIzLCJpYXQiOjE1NjQ3MzAwNzksImV4cCI6MTAwMTU2NDczMDA3OH0.YPt-XeLnjV-_1ITaXGY2FhxmCe4NvXuRnRB8OMCfnPo"
+    const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjIzLCJpYXQiOjE1NjQ3MzAwNzksImV4cCI6MTAwMTU2NDczMDA3OH0.YPt-XeLnjV-_1ITaXGY2FhxmCe4NvXuRnRB8OMCfnPo"
     wx.setStorageSync("token", token);
     wx.navigateBack({
       delta: 1
